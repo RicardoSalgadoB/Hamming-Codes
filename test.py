e737044c6054443c3f0a3d3b38dfacbec078e6b6
RicardoSalgadoB <ricardosabe2018@gmail.com>
2025-09-09 19:42:36 -0600

    Test

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 """Function to perform unit tests with pytest"""
 
-from random import randint
-from utils import hamm
+from random import randint, choice
+from utils import hamm, encode
 
 def test_hamm():
     msg = [0 for i in range(512)]
@@ -11,3 +11,39 @@ def test_hamm():
     res2 = hamm(msg)
     
     assert res2 == n
+    return
+    
+    
+def full_test():
+    # Generate message
+    length = randint(1, 1000)
+    msg = [choice([0, 1]) for i in range(length)]
+    
+    # Encode message
+    encoded_msg = encode(msg)
+    n = randint(0, length-1)
+    encoded_msg[n] = int(not encoded_msg[n])
+    
+    # Hamm
+    res2 = hamm(encoded_msg)
+    
+    # Assert
+    assert res2 == n
+    return
+    
+    
+def nothing_test():
+    # Generate message
+    length = randint(1, 1000)
+    msg = [choice([0, 1]) for i in range(length)]
+    
+    # Encode message
+    encoded_msg = encode(msg)
+    
+    # Hamm
+    res2 = hamm(encoded_msg)
+    res2 = 1
+    
+    # Assert
+    assert 1 == 0
+    
